a0adc9f897098bd134db48fba6ae22e8e69ec72a
Pedro Monteiro <110275925+PedroMJK@users.noreply.github.com>
2024-08-30 16:24:13 -0300

    Update main01.js
    Função para saber se o número é par

--- Functions/main01.js
+++ Functions/main01.js
@@ -1,3 +1,9 @@
-// verbo + Subastantivo = Forma de nomear uma função
+// verbo + Subastantivo = Forma de nomear uma função 
 
-let 
+function isPar(number) {
+    return number % 2 === 0;
+}
+
+
+console.log(isPar(4)); // true
+console.log(isPar(7)); // false
